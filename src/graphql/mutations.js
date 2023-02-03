b063d1201fb232c61d8070795691e06beaa4e4cd
Manoj Thapa <mnjinvincible@gmail.com>
2023-02-03 20:46:49 +0545

    changed public auth provider to iam

---
+++ src/graphql/mutations.js
@@ -0,0 +1,99 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createCourse = /* GraphQL */ `
+  mutation CreateCourse(
+    $input: CreateCourseInput!
+    $condition: ModelCourseConditionInput
+  ) {
+    createCourse(input: $input, condition: $condition) {
+      id
+      title
+      subtitle
+      category
+      files
+      price
+      language
+      tutor
+      tutorWho
+      relatedSkills
+      youLearn
+      duration
+      framework
+      reqKnowledge
+      level
+      time
+      courseContent {
+        topic
+        description
+        duration
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateCourse = /* GraphQL */ `
+  mutation UpdateCourse(
+    $input: UpdateCourseInput!
+    $condition: ModelCourseConditionInput
+  ) {
+    updateCourse(input: $input, condition: $condition) {
+      id
+      title
+      subtitle
+      category
+      files
+      price
+      language
+      tutor
+      tutorWho
+      relatedSkills
+      youLearn
+      duration
+      framework
+      reqKnowledge
+      level
+      time
+      courseContent {
+        topic
+        description
+        duration
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteCourse = /* GraphQL */ `
+  mutation DeleteCourse(
+    $input: DeleteCourseInput!
+    $condition: ModelCourseConditionInput
+  ) {
+    deleteCourse(input: $input, condition: $condition) {
+      id
+      title
+      subtitle
+      category
+      files
+      price
+      language
+      tutor
+      tutorWho
+      relatedSkills
+      youLearn
+      duration
+      framework
+      reqKnowledge
+      level
+      time
+      courseContent {
+        topic
+        description
+        duration
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
